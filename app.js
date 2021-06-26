a444e180ce1683a6b3a2041232ac44c071380c99
Mu$hr00M <71194731+Mushroom0047@users.noreply.github.com>
2021-06-25 23:25:01 -0400

    Correction of routes

--- app.js
+++ app.js
@@ -1,41 +1,56 @@
-const express = require('express');
-const mysql = require('mysql');
-const bodyParser = require('body-parser');
-//const PORT = process.env.PORT || 3050;
+const express = require("express");
+const mysql = require("mysql");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const PORT = process.env.PORT || 3050;
 
 const app = express();
 
-app.use(bodyParser.json())
+app.use(bodyParser.json());
+app.use(cors());
 
 //mysql
 var connection = mysql.createConnection({
-    host: "localhost",
-    database:"hectorva_db",
-    user: "hectorva_user01",
-    password: "codname47",
-    //user: "root",
-    //password: "",
-    charset : "utf8mb4"
+  host: "localhost",
+  database: "hectorva_db",
+  user: "hectorva_user01",
+  password: "codname47",
+  charset: "utf8mb4",
+});
+
+//rutas
+// Return All quotes
+app.get("/api/frases-motiv/", (req, res) => {
+  const sql = `SELECT * FROM FrasesMotiv`;
+  connection.query(sql, (error, result) => {
+    if (error) throw error;
+    if (result.length > 0) {
+      res.json(result);
+    } else {
+      res.send("NOt results");
+    }
   });
+});
 
- //rutas
- app.get('/frases-motiv/:id', (req, res)=>{
-     const {id }= req.params;
-     const sql = `SELECT * FROM FrasesMotiv WHERE id = ${id}`;
-     connection.query(sql, (error, result)=>{
-        if(error) throw error;
-        if(result.length > 0){
-            res.json(result);
-        }else{
-            res.send('NOt results');
-        }
-    });
- });
-
-  //chceck conn
-  connection.connect(error =>{
-      if(error) throw error;
-      console.log('Database is runing');
+// Return quotes by id
+app.get("/app/frases-motiv/:id", (req, res) => {
+  const { id } = req.params;
+  const sql = `SELECT * FROM FrasesMotiv WHERE id = ${id}`;
+  connection.query(sql, (error, result) => {
+    if (error) throw error;
+    if (result.length > 0) {
+      res.json(result);
+    } else {
+      res.send("NOt results");
+    }
   });
+});
 
-  //app.listen(PORT, ()=> console.log(`Server running on port ${PORT}`));
+
+//chceck conn
+connection.connect((error) => {
+  if (error) throw error;
+  console.log("Database is runing");
+});
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
